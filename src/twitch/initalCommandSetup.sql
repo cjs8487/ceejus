672212086f09a4e04217c3d3aafdd4a9e5dc87ee
cjs8487 <cjschaefer1999@gmail.com>
2021-03-15 22:10:31 -0400

    more inital comands

--- src/twitch/initalCommandSetup.sql
+++ src/twitch/initalCommandSetup.sql
@@ -1,4 +1,8 @@
 -- SQLite
 insert into COMMANDS (command_string, output) values
     ('discord', 'Join the Discord for stream announcements and more! https://discord.gg/5CmuYtT'),
-    ('ssr', 'https://discord.gg/evpNKkaaw6');
+    ('ssr', 'https://discord.gg/evpNKkaaw6'),
+    ('twitter', 'Follow me on Twitter: https://twitter.com/cjs078'),
+    ('settings', 'No settings specifed. Mods please fix'),
+    ('race', 'No race channel specified'),
+    ('hints', 'No hints specified. Mods please fix if appropriate, or disable the command if not');
