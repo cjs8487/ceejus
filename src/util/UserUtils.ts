4c2efe415c37c15c9883d4bdaf5b6d2800c51d06
cjs8487 <cjschaefer1999@gmail.com>
2023-10-02 21:57:07 -0500

    switch userutils to universal apiclient

--- src/util/UserUtils.ts
+++ src/util/UserUtils.ts
@@ -1,10 +1,10 @@
-import { botApiClient } from '../System';
+import { apiClient } from '../auth/TwitchAuth';
 import { getUserByTwitchId, registerUserWithoutAuth } from '../database/Users';
 
 export const getOrCreateUserId = async (twitchId: string): Promise<number> => {
     const user = getUserByTwitchId(twitchId).userId;
     if (user === -1) {
-        const username = (await botApiClient.users.getUserById(twitchId))?.name;
+        const username = (await apiClient.users.getUserById(twitchId))?.name;
         if (username === undefined) return -1;
         return registerUserWithoutAuth(username, twitchId);
     }
@@ -14,7 +14,7 @@ export const getOrCreateUserId = async (twitchId: string): Promise<number> => {
 export const getOrCreateUserName = async (
     username: string,
 ): Promise<number> => {
-    const twitchId = (await botApiClient.users.getUserByName(username))?.id;
+    const twitchId = (await apiClient.users.getUserByName(username))?.id;
     if (twitchId === undefined) return -1;
     const user = getUserByTwitchId(twitchId).userId;
     if (user === -1) {
