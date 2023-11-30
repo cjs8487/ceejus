c96a6da5cbef796a32a9ac0294bc81d0da7ceb2e
cjs8487 <cjschaefer1999@gmail.com>
2023-11-29 21:05:53 -0600

    product card p1

--- frontend/src/routes/store/Store.tsx
+++ frontend/src/routes/store/Store.tsx
@@ -3,7 +3,19 @@ import { useParams } from 'react-router-dom';
 const Store = () => {
     const { user } = useParams();
 
-    return <>{user} owns this store</>;
+    return (
+        <div>
+            {user} owns this store
+            <div className="flex">
+                <div className="flex flex-col items-center justify-center gap-y-2 rounded-md border shadow-md">
+                    <div className="h-24 w-full rounded-t-md bg-orange-600"></div>
+                    <div className="px-4">Product name</div>
+                    <div>product type</div>
+                    <div>1000 BiTcoins</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Store;
